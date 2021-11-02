70793cc39e5a37c6e91c879032bcef346454e897
MS-LEGACY <shukla.mrigank.19@gmail.com>
2021-11-02 13:15:04 +0530

    Changed cert requirement to test heroku deployment

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const { join } = require('path')
 const app = express()
 const fs=require('fs')
 const path=require('path')
-
-const server = require('https').createServer({ 
-    key:fs.readFileSync(path.join(__dirname,'cert','key.pem')), //mention your path to key
-    cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))},app)//.Server(app) //mention your path to cert
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+// const server = require('https').createServer({ 
+//     key:fs.readFileSync(path.join(__dirname,'cert','key.pem')), //mention your path to key
+//     cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))},app)//.Server(app) //mention your path to cert
 
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
