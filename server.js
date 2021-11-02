e13447a3791187a5891ba745dc7edd376da8d393
MS-LEGACY <shukla.mrigank.19@gmail.com>
2021-11-02 13:22:53 +0530

    Some variables correction

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const path=require('path')
 const server = require('http').Server(app)
 //const io = require('socket.io')(server)
 //const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-  debug: true
-});
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true
+// });
 // const server = require('https').createServer({ 
 //     key:fs.readFileSync(path.join(__dirname,'cert','key.pem')), //mention your path to key
 //     cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))},app)//.Server(app) //mention your path to cert
